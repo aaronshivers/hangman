695cfc9e44a2ecb9b8a65371a0c09fc40d563fb6
aaronshivers <aaron@moonmail.org>
2019-08-02 19:43:39 -0500

    setup webpack for development

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,31 @@
 const path = require('path')
 
-module.exports = {
-  entry: ['@babel/polyfill', './src/index.js'],
-  output: {
-    path: path.resolve(__dirname, 'public/scripts')
-  },
-  module: {
-    rules: [{
-      test: /\.m?js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env']
+module.exports = env => {
+  console.log(env)
+
+  return {
+    entry: ['@babel/polyfill', './src/index.js'],
+    output: {
+      path: path.join(__dirname, 'public', 'dist')
+    },
+    module: {
+      rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
         }
-      }
-    }]
-  },
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    publicPath: '/scripts/'
-  },
-  devtool: 'source-map'
+      ]
+    },
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      publicPath: '/dist/'
+    },
+    devtool: 'source-map'
+  }
 }
